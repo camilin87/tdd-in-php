d32eca7a6e7c908643a946729db377f94ffbbb25
camilin87 <camilin87@gmail.com>
2016-04-10 10:10:30 -0400

    add a new failure

--- tests/fibonacciCalculatorTest.php
+++ tests/fibonacciCalculatorTest.php
@@ -2,12 +2,20 @@
     require "src/fibonacciCalculator.php";
 
     class FibonacciCalculatorTest extends PHPUnit_Framework_TestCase{
-        public function testFibonacci(){
+        public function testSecondFibonacciNumber(){
             $c = new FibonacciCalculator;
 
             $n = $c->Get(1);
 
             $this->assertEquals(1, $n);
+        }
+
+        public function testFirstFibonacciNumber(){
+            $c = new FibonacciCalculator;
+
+            $n = $c->Get(0);
+
+            $this->assertEquals(0, $n);
         }
     }
 ?>
